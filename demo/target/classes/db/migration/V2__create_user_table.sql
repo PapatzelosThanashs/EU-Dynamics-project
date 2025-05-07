67f834971f7463cc326774f9d676ae35c6469789
sakis <papatzelosthanashs@gmail.com>
2025-05-07 18:25:44 +0300

    add flyway for migrations

---
+++ demo/target/classes/db/migration/V2__create_user_table.sql
@@ -0,0 +1,14 @@
+-- Create User table
+CREATE TABLE user (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    surname VARCHAR(12) NOT NULL,
+    birthdate DATE NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    address_id BIGINT UNIQUE,
+    CONSTRAINT fk_user_address
+        FOREIGN KEY (address_id)
+        REFERENCES address(id)
+        ON DELETE CASCADE
+);
+
