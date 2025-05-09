3824e8124305bec1ed28e2dcba81c4c78a1adeed
sakis <papatzelosthanashs@gmail.com>
2025-05-09 20:39:44 +0300

    add axios + DisplayUsers getUsers()

--- frontend/src/views/DisplayUsers.vue
+++ frontend/src/views/DisplayUsers.vue
@@ -1,13 +1,48 @@
 <template>
     <div class="displayusers">
     
-    <p>hello</p>
+        <table border="1">
+            <thead>
+                <tr>
+                <th>Name</th>
+                <th>Surname</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user in users" :key="user.id" >
+                <td>{{ user.name }}</td>
+                <td>{{ user.surname }}</td>
+                </tr>
+            </tbody>
+        </table>
   
     </div>
     </template>
   <script>
+
+import axios from "axios";
+
   export default {
     name: 'DisplayUsers',
+    data(){
+    return{ 
+      users:[]
+    };
+  },
+    methods: {
+        async getUsers() {
+        try {
+            const response = await axios.get("http://localhost:8081/api/users");
+            this.users = response.data;
+        } catch (error) {
+            console.error("Error fetching users:", error);
+            alert("Failed to load users.");
+        }
+        }
+  },
+    mounted(){
+        this.getUsers();
+    }
   }
   </script>
   
