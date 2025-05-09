974d3bd4ccdd680b4cdd0d4ad4f0996327a3f7d5
sakis <papatzelosthanashs@gmail.com>
2025-05-09 21:09:09 +0300

    add deleteUser function in DisplayUsers

--- frontend/src/views/DisplayUsers.vue
+++ frontend/src/views/DisplayUsers.vue
@@ -12,6 +12,7 @@
                 <tr v-for="user in users" :key="user.id" >
                 <td>{{ user.name }}</td>
                 <td>{{ user.surname }}</td>
+                <td><button @click="deleteUser(user.id)">Delete User</button></td>
                 </tr>
             </tbody>
         </table>
@@ -33,12 +34,26 @@ import axios from "axios";
         async getUsers() {
         try {
             const response = await axios.get("http://localhost:8081/api/users");
+           
             this.users = response.data;
+
         } catch (error) {
             console.error("Error fetching users:", error);
             alert("Failed to load users.");
         }
+        },
+        async deleteUser(userId) {
+      if (confirm("Are you sure you want to delete this user?")) {
+        try {
+          await axios.delete(`http://localhost:8081/api/users/${userId}`);
+          this.users = this.users.filter(user => user.id !== userId);
+          alert("User successfully deleted");
+        } catch (error) {
+          console.error("Error deleting user:", error);
+          alert("Delete failed.");
         }
+      }
+    }
   },
     mounted(){
         this.getUsers();
