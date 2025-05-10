7d57ab6e8dba26d2f3109a9a88821addd1db25e2
sakis <papatzelosthanashs@gmail.com>
2025-05-10 21:06:21 +0300

    add views/NotFound.vue + router path

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './views/HomePage.vue'
 import DisplayUsers  from './views/DisplayUsers.vue'
 import RegisterUser  from './views/RegisterUser.vue'
+import NotFound from './views/NotFound.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/users', component: DisplayUsers },
   { path: '/register', component: RegisterUser},
+  { path: '/:pathMatch(.*)*', component: NotFound},
 ]
 
 const router = createRouter({
