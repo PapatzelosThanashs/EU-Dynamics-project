6cfdcce88239141d6c902f6563b3b062f7ee1a63
sakis <papatzelosthanashs@gmail.com>
2025-05-13 12:00:21 +0300

    add .gitignore

--- demo/target/classes/db/migration/V1__create_address_table.sql
+++
@@ -1,6 +0,0 @@
--- Create Address table first because User depends on it
-CREATE TABLE address (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    home_address VARCHAR(255),
-    work_address VARCHAR(255)
-);
