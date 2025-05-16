da5425f15ba2f10a5dfa3520bf6612264f0355a3
sakis <papatzelosthanashs@gmail.com>
2025-05-16 09:42:49 +0300

    add Swagger OpenAPI 3 annoattions in controller/UserController

--- demo/src/main/java/com/project/demo/controller/UserController.java
+++ demo/src/main/java/com/project/demo/controller/UserController.java
@@ -19,14 +19,19 @@ import org.springframework.web.bind.annotation.CrossOrigin; //<--- delete this. 
 import com.project.demo.dto.validation.OnCreate;
 import com.project.demo.dto.validation.OnPatch;
 import com.project.demo.dto.UserSummaryDTO;
+// OpenAPI 3/swagger annotations
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Parameter;
+
 
 
 
 @CrossOrigin(origins = "http://localhost:8080") 
 
 @RestController
-
 @RequestMapping("api/users")
+@Tag(name = "User Management", description = "Operations related to user management")
 public class UserController {
 
     private UserService userService;
@@ -35,31 +40,38 @@ public class UserController {
         this.userService= userService;
     } 
 
+    
+
     /* Get all users */
+    @Operation(summary = "Get all users", description = "Returns a summary list of all users, only id, name and surname are returned")
     @GetMapping
     public List<UserSummaryDTO> all() {
         return userService.findAll();
     }
 
     /* Create new user */
+    @Operation(summary = "Create a new user", description = "Creates a new user with the provided details")
     @PostMapping
     public UserDTO create(@RequestBody @Validated(OnCreate.class) UserDTO userDTO) {
         return userService.saveUser(userDTO);
     }
 
     /* Get user with id=?*/
+    @Operation(summary = "Get user by ID", description = "Fetches user details for the given ID")
     @GetMapping("/{id}")
     public UserDTO get(@PathVariable Long id) {
         return userService.findUser(id);
     }
 
     /* Delete user with id=?*/
+    @Operation(summary = "Delete user by ID", description = "Deletes the user for the given ID")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
          userService.deleteUser(id);
     }
 
     /* Patch user */
+    @Operation(summary = "Partially update a user", description = "Updates specific fields of a user using PATCH, version field is mandatory")
     @PatchMapping("/{id}")
     public UserDTO patch(@PathVariable Long id, @RequestBody @Validated(OnPatch.class) UserDTO userDTO) {
       return  userService.patchUser(id,userDTO);
@@ -67,6 +79,7 @@ public class UserController {
     }
 
       /* Update user */
+    @Operation(summary = "Fully update a user", description = "Replaces all user details with provided data")
     @PutMapping("/{id}")
     public UserDTO put(@PathVariable Long id, @RequestBody @Validated(OnCreate.class) UserDTO userDTO) {
         return userService.updateUser(id, userDTO);
